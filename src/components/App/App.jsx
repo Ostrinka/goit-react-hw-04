14b97908b3778d66381b5a5faade7cb35c1a379c
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-19 21:21:03 +0300

    add Loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,18 @@ import { Toaster, toast } from 'react-hot-toast';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import Loader from '../Loader/Loader';
 import { getImages } from '../../images-api';
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState();
 
   const fetchImages = async (searchQuery) => {
     try {
+      setLoading(true);
       const newImages = await getImages(searchQuery, 1);
       setImages(newImages);
       setQuery(searchQuery);
@@ -20,16 +23,23 @@ export default function App() {
       console.error(error);
       toast.error('Failed to fetch images');
     }
+      finally {
+        setLoading(false);
+    }
   };
 
   const loadMoreImages = async () => {
     try {
+      setLoading(true);
       const newImages = await getImages(query, currentPage + 1);
       setImages((prevImages) => [...prevImages, ...newImages]);
       setCurrentPage((prevPage) => prevPage + 1);
     } catch (error) {
       console.error(error);
       toast.error('Failed to fetch more images');
+    }
+    finally {
+      setLoading(false);
     }
   };
 
@@ -39,6 +49,7 @@ export default function App() {
       {images.length > 0 && (
         <>
           <ImageGallery images={images} />
+          {loading && <Loader />}
           <LoadMoreBtn loadMoreImages={loadMoreImages} />
         </>
       )}
